d81f8e354fd1c9053be1f3f16dc0ff7c3f1f1c08
Jarosław Szewczyk <jaroslaw.szewczyk@tutanota.com>
2020-04-22 19:13:23 +0100

    Add Kitchen and Home components

--- src/components/views/Home/Home.js
+++ src/components/views/Home/Home.js
@@ -1,10 +1,58 @@
 import React from 'react';
 import styles from './Home.module.scss';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
-const Dashboard = () => (
-  <div className={styles.component}>
-    <h2>Home</h2>
-  </div>
+const Home = () => (
+  <Paper>
+    <Table className={styles.margin}>
+      <TableHead>
+        <TableRow>
+          <TableCell colSpan={3} align="center">Orders </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell></TableCell>
+          <TableCell align="center">Amount</TableCell>
+          <TableCell align="center">Revenue</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell align="center">Restaurant</TableCell>
+          <TableCell align="center">10</TableCell>
+          <TableCell align="center">$123.57</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell align="center">Delivery</TableCell>
+          <TableCell align="center">5</TableCell>
+          <TableCell align="center">$250.78</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell colSpan={2} align="center">Reservation and events</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell align="center">Reservation</TableCell>
+          <TableCell align="center">events</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableRow>
+          <TableCell align="center">3</TableCell>
+          <TableCell align="center">1</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+
+  </Paper>
 );
 
-export default Dashboard;
+export default Home;
--- src/components/views/Kitchen/Kitchen.js
+++ src/components/views/Kitchen/Kitchen.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import styles from './Kitchen.module.scss';
 
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+
 const Kitchen = () => (
-  <div className={styles.component}>
-    <h2>Kitchen</h2>
-  </div>
+  <Paper>
+    <List dense>
+      {[0, 1, 2].map((value) => {
+        const labelId = `checkbox-list-secondary-label-${value}`;
+        return (
+          <ListItem key={value} button>
+            <ListItemText id={labelId} primary={`${value + 1}`} />
+            <ListItemText id={labelId} primary={`Table no. ${value + 1}`} />
+            <ListItemText id={labelId} primary={`item ${value + 1}`} />
+            <ListItemSecondaryAction>
+              <Checkbox
+                edge="end"
+              />
+            </ListItemSecondaryAction>
+          </ListItem>
+        );
+      })}
+    </List>
+  </Paper>
 );
 
 export default Kitchen;
